22cb6de5cc9737ab37659f4f07c9f853ce725435
Rainsong <eder.gabriel.felix@gmail.com>
2018-09-21 21:51:00 -0300

    Working Peer pinging and discovery

--- swarm.py
+++ swarm.py
@@ -7,39 +7,85 @@ s_print_lock = threading.Lock()
 
 def s_print(*a, **b):
     """Thread safe print function"""
+    #print('aaaa')
     with s_print_lock:
         print(*a, **b)
-def listen_socket(udp_socket, up_queue, down_queue):
-    sprint('aaaa')
+    sys.stdout.flush()
+
+def listen_thread(udp_socket, read_queue, write_queue):
+    s_print('Started listen_thread', flush=True)
     while True:
-        data, addr = sock.recvfrom(1024)
-        data = data.decode('utf-8')
-        op, *args, hash = data.split('|')
+        try:
+            data, addr = udp_socket.recvfrom(1024)
+            s_print('[LISTEN] Incoming data from', addr, data)
+            data = data.decode('utf-8')
+            op, *args = data.split('|')
 
-        print(op, args, hash)
+            if op == 'PING':
+                response = b'PONG\n'
+                s_print('[LISTEN] Answering', addr , 'with', response)
+                udp_socket.sendto(response, addr)
 
-        if op == 'PING':
-            print('PONG')
-            sock.sendto(b'PONG\n', addr)
-
-        if op == 'DSCVR':
-            sock.sendto(b'||127.0.0.1', addr)
+            if op == 'DSCVR':
+                response = b'||127.0.0.1'
+                s_print('[LISTEN] Answering', addr , 'with', response)
+                write_queue.put((args[0], int(args[1])))
+                udp_socket.sendto(response, addr)
+        except Exception as e:
+            print(e)
 
 def boot_server(host='127.0.0.1', port=2048):
-    MESSAGE = "Hello, World!"
+
+    SEEDS = [
+        ('127.0.0.1', 1998),
+        ('127.0.0.1', 1999),
+        ('127.0.0.1', 2000)
+    ]
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((host, port))
 
-    up_queue = queue.Queue()
-    down_queue = queue.Queue()
+    listenthread_read_queue = queue.Queue()
+    listenthread_write_queue = queue.Queue()
 
-    t = threading.Thread(target=listen_socket, args=(sock, up_queue, down_queue))
-    while True:
-        print(1)
-        time.sleep(1)
+    t = threading.Thread(target=listen_thread, args=(sock, listenthread_write_queue, listenthread_read_queue))
+    t.setDaemon(True)
+    t.start()
+
+    peers = []
+
+    for peer in SEEDS:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.sendto(b'PING|',('127.0.0.1', 2048))
+        s.sendto(f'DSCVR|{host}|{port}'.encode(), peer)
+        print('Contacting', peer)
+        peers.append(peer)
+
+    while True:
+        for peer in peers:
+            try:
+                s_print('[KEEPALIVE] Pinging to', peer)
+
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.settimeout(1)
+                s.sendto(b'PING|\n', peer)
+                s.recv(1024)
+                s_print('[KEEPALIVE]', peer, 'is alive', )
+
+            except Exception as e:
+                #print('Failed', e)
+                #TODO remove peer from list
+                #print(e)
+                pass
+
+            # Get peers
+            try:
+                new_peer = listenthread_read_queue.get_nowait()
+                s_print('[DISCOVERY] adding', new_peer , 'to peer list')
+                peers.append(new_peer)
+            except queue.Empty as e:
+                pass
+
+        time.sleep(5)
 
 if __name__ == '__main__':
     boot_server(port=int(sys.argv[1]))
